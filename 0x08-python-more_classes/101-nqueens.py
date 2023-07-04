c00bf4b1acc26ab21456c478b0bdce1f8aa56750
Danielikye <iwuegbulemdanielikechukwu@gmail.com>
2023-07-04 01:18:45 -0700

    updated

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,62 +1,107 @@
 #!/usr/bin/python3
+"""calculates and return the area of the rectangle
 """
-nqueens backtracking program to print the coordinates of n queens
-on an nxn grid such that they are all in non-attacking positions
-"""
+import sys
+
+'''
+File_name: 101-nqueens.py
+Created: 5th of June, 2023
+Auth: David James Taiye (Official0mega)
+Size: undefined
+Project: 0x08-python-more_classes
+Status: submitted.
+'''
 
 
-from sys import argv
+def is_safe(board, row, col):
+    """
+    # Chess grandmaster Judit Polgár, the strongest female chess...
+    # player of all time...The N queens puzzle is the challenge of....
+    # ....placing N non-attacking queens on an N×N chessboard...
+    # Write a program that solves the N queens problem.....
+    # VARIABLE(" "):
+    # Return: Always 0, (Success).
+    """
+    """ Check if it's safe to place a queen at board[row][col]
+    """
 
-if __name__ == "__main__":
-    a = []
-    if len(argv) != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
-    n = int(argv[1])
-    if n < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    # initialize the answer list
-    for i in range(n):
-        a.append([i, None])
-
-    def already_exists(y):
-        """check that a queen does not already exist in that y value"""
-        for x in range(n):
-            if y == a[x][1]:
-                return True
-        return False
-
-    def reject(x, y):
-        """determines whether or not to reject the solution"""
-        if (already_exists(y)):
+    # Check row on the left side
+    for i in range(col):
+        if board[row][i] == 1:
             return False
-        i = 0
-        while(i < x):
-            if abs(a[i][1] - y) == abs(i - x):
-                return False
-            i += 1
+
+    # Check upper diagonal on the left side
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+
+    # Check lower diagonal on the left side
+    i, j = row, col
+    while i < N and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i += 1
+        j -= 1
+
+    return True
+
+
+def solve_nqueens(board, col):
+    """ Solve the N-Queens problem using backtracking
+    """
+
+    # Base case: If all queens are placed
+    if col >= N:
+        print_solution(board)
         return True
 
-    def clear_a(x):
-        """clears the answers from the point of failure on"""
-        for i in range(x, n):
-            a[i][1] = None
+    # Consider this column and try placing a queen in all rows
+    for i in range(N):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            """Place the queen"""
 
-    def nqueens(x):
-        """recursive backtracking function to find the solution"""
-        for y in range(n):
-            clear_a(x)
-            if reject(x, y):
-                a[x][1] = y
-                if (x == n - 1):  # accepts the solution
-                    print(a)
-                else:
-                    nqueens(x + 1)  # moves on to next x value to continue
+        # Recur to place the rest of the queens
+        solve_nqueens(board, col + 1)
 
-    # start the recursive process at x = 0
-    nqueens(0)
+        # Backtrack and remove the queen from this cell
+        board[i][col] = 0
+
+
+def print_solution(board):
+    """ Print the solution in the required format
+    """
+
+    solution = []
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+if __name__ == "__main__":
+    # Check if the number of arguments is correct
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    # Check if N is at least 4
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    # Initialize an empty chessboard
+    board = [[0 for _ in range(N)] for _ in range(N)]
+
+    # Solve the N-Queens problem
+    solve_nqueens(board, 0)
